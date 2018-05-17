965fc1508e601c6ab3daccae61d69e7cd03d9634
Yeonjae Park <yeonjae.park.19@dartmouth.edu>
2018-05-17 18:14:18 -0400

    working 2

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 var x = 0,
-    container = $('.container'),
-    items = container.find('li'),
+    titles = $('.titles'),
+    items = titles.find('li'),
     containerHeight = 0,
     numberVisible = 5,
     intervalSec = 2000;
 
-if(!container.find('li:first').hasClass("first")){
-  container.find('li:first').addClass("first");
+if(!titles.find('li:first').hasClass("first")){
+  titles.find('li:first').addClass("first");
 }
 
 items.each(function(){
-  if(x < numberVisible){
+  if(x < numberVisible) {
     containerHeight = containerHeight + $(this).outerHeight();
     x++;
   }
 });
 
-container.css({ height: containerHeight, overflow: "hidden" });
-  
+titles.css({ height: containerHeight, overflow: "hidden" });
+
 function vertCycle() {
-  var firstItem = container.find('li.first').html();
-    
-  container.append('<li>'+firstItem+'</li>');
+  var firstItem = titles.find('li.first').html();
+
+  titles.append('<li>'+firstItem+'</li>');
   firstItem = '';
-  container.find('li.first').animate({ marginTop: "-50px" }, 600, function(){  $(this).remove(); container.find('li:first').addClass("first"); });
+  titles.find('li.first').animate({ marginTop: "-50px" }, 600, function(){  $(this).remove(); titles.find('li:first').addClass("first"); });
 }
 
 if(intervalSec < 700){
@@ -32,8 +32,49 @@ if(intervalSec < 700){
 
 var init = setInterval("vertCycle()",intervalSec);
 
-container.hover(function(){
+
+// articles one
+var y = 0,
+    articles = $('.articles'),
+    articleItems = articles.find('li'),
+    articlesContainerHeight = 0,
+    articlesNumVis = 1
+
+if(!articles.find('li:first').hasClass("first")){
+  articles.find('li:first').addClass("first");
+}
+
+articleItems.each(function(){
+  if(y < articlesNumVis) {
+    articlesContainerHeight = articlesContainerHeight + $(this).outerHeight();
+    y++;
+  }
+});
+
+articles.css({ height: articlesContainerHeight, overflow: "hidden" });
+
+function articlesCycle() {
+  var firstItem = articles.find('li.first').html();
+
+  articles.append('<li>'+firstItem+'</li>');
+  firstItem = '';
+  articles.find('li.first').animate({ marginTop: "-50px" }, 600, function(){  $(this).remove(); articles.find('li:first').addClass("first"); });
+}
+
+var articlesInit = setInterval("articlesCycle()",intervalSec);
+
+articles.hover(function(){
+  clearInterval(articlesInit);
   clearInterval(init);
 }, function(){
   init = setInterval("vertCycle()",intervalSec);
+  articlesInit = setInterval("articlesCycle()",intervalSec);
+});
+
+titles.hover(function(){
+  clearInterval(articlesInit);
+  clearInterval(init);
+}, function(){
+  init = setInterval("vertCycle()",intervalSec);
+  articlesInit = setInterval("articlesCycle()",intervalSec);
 });
