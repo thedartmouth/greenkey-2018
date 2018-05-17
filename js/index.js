472faa331600ac450d894d68d449b27d33ff7724
Yeonjae Park <yeonjae.park.19@dartmouth.edu>
2018-05-17 19:22:44 -0400

    adding mobile

--- js/index.js
+++ js/index.js
@@ -78,3 +78,13 @@ titles.hover(function(){
   init = setInterval("vertCycle()",intervalSec);
   articlesInit = setInterval("articlesCycle()",intervalSec);
 });
+
+// mobile
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
