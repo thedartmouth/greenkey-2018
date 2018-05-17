d79f70cc020dad231f4b745ffced07a1ac09b02d
Yeonjae Park <yeonjae.park.19@dartmouth.edu>
2018-05-17 19:50:27 -0400

    final

--- js/index.js
+++ js/index.js
@@ -1,90 +1,84 @@
-var x = 0,
-    titles = $('.titles'),
-    items = titles.find('li'),
-    containerHeight = 0,
-    numberVisible = 5,
-    intervalSec = 5000;
+if ($(window).width() > 480) {
+  var x = 0,
+      titles = $('.titles'),
+      items = titles.find('li'),
+      containerHeight = 0,
+      numberVisible = 5,
+      intervalSec = 5000;
 
-if(!titles.find('li:first').hasClass("first")){
-  titles.find('li:first').addClass("first");
-}
-
-items.each(function(){
-  if(x < numberVisible) {
-    containerHeight = containerHeight + $(this).outerHeight();
-    x++;
+  if(!titles.find('li:first').hasClass("first")){
+    titles.find('li:first').addClass("first");
   }
-});
 
-titles.css({ height: containerHeight, overflow: "hidden" });
-
-function vertCycle() {
-  var firstItem = titles.find('li.first').html();
-
-  titles.append('<li>'+firstItem+'</li>');
-  firstItem = '';
-  titles.find('li.first').animate({ marginTop: "-50px" }, 1000, function(){  $(this).remove(); titles.find('li:first').addClass("first"); });
-}
-
-if(intervalSec < 700){
-  intervalSec = 700;
-}
-
-var init = setInterval("vertCycle()",intervalSec);
-
-
-// articles one
-var y = 0,
-    articles = $('.articles'),
-    articleItems = articles.find('li'),
-    articlesContainerHeight = 0,
-    articlesNumVis = 1
-
-if(!articles.find('li:first').hasClass("first")){
-  articles.find('li:first').addClass("first");
-}
-
-articleItems.each(function(){
-  if(y < articlesNumVis) {
-    articlesContainerHeight = articlesContainerHeight + $(this).outerHeight();
-    y++;
-  }
-});
-
-articles.css({ height: articlesContainerHeight, overflow: "hidden" });
-
-function articlesCycle() {
-  var firstItem = articles.find('li.first').html();
-
-  articles.append('<li>'+firstItem+'</li>');
-  firstItem = '';
-  articles.find('li.first').animate({ marginTop: "-700px" }, 1000, function(){  $(this).remove(); articles.find('li:first').addClass("first"); });
-}
-
-var articlesInit = setInterval("articlesCycle()",intervalSec);
-
-articles.hover(function(){
-  clearInterval(articlesInit);
-  clearInterval(init);
-}, function(){
-  init = setInterval("vertCycle()",intervalSec);
-  articlesInit = setInterval("articlesCycle()",intervalSec);
-});
-
-titles.hover(function(){
-  clearInterval(articlesInit);
-  clearInterval(init);
-}, function(){
-  init = setInterval("vertCycle()",intervalSec);
-  articlesInit = setInterval("articlesCycle()",intervalSec);
-});
-
-// mobile
-$(document).ready(function(){
-  $('.slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  items.each(function(){
+    if(x < numberVisible) {
+      containerHeight = containerHeight + $(this).outerHeight();
+      x++;
+    }
   });
-});
+
+  titles.css({ height: containerHeight, overflow: "hidden" });
+
+  function vertCycle() {
+    var firstItem = titles.find('li.first').html();
+
+    titles.append('<li>'+firstItem+'</li>');
+    firstItem = '';
+    titles.find('li.first').animate({ marginTop: "-50px" }, 1000, function(){  $(this).remove(); titles.find('li:first').addClass("first"); });
+  }
+
+  if(intervalSec < 700){
+    intervalSec = 700;
+  }
+
+  var init = setInterval("vertCycle()",intervalSec);
+
+
+  // articles one
+  var y = 0,
+      articles = $('.articles'),
+      articleItems = articles.find('li'),
+      articlesContainerHeight = 0,
+      articlesNumVis = 1
+
+  if(!articles.find('li:first').hasClass("first")){
+    articles.find('li:first').addClass("first");
+  }
+
+  articleItems.each(function(){
+    if(y < articlesNumVis) {
+      articlesContainerHeight = articlesContainerHeight + $(this).outerHeight();
+      y++;
+    }
+  });
+
+  articles.css({ height: articlesContainerHeight, overflow: "hidden" });
+
+  function articlesCycle() {
+    var firstItem = articles.find('li.first').html();
+
+    articles.append('<li>'+firstItem+'</li>');
+    firstItem = '';
+    articles.find('li.first').animate({ marginTop: "-700px" }, 1000, function(){  $(this).remove(); articles.find('li:first').addClass("first"); });
+  }
+
+  var articlesInit = setInterval("articlesCycle()",intervalSec);
+
+  articles.hover(function(){
+    clearInterval(articlesInit);
+    clearInterval(init);
+  }, function(){
+    init = setInterval("vertCycle()",intervalSec);
+    articlesInit = setInterval("articlesCycle()",intervalSec);
+  });
+
+  titles.hover(function(){
+    clearInterval(articlesInit);
+    clearInterval(init);
+  }, function(){
+    init = setInterval("vertCycle()",intervalSec);
+    articlesInit = setInterval("articlesCycle()",intervalSec);
+  });
+} else {
+  $(".titles").addClass("hidden");
+}
